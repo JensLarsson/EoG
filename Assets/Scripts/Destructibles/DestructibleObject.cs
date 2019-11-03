c553511c8862818521e290b7868690806e3499c5
Ekkara <47211128+Ekkara@users.noreply.github.com>
2019-11-03 19:24:41 +0100

    base of destructible object

---
+++ Assets/Scripts/Destructibles/DestructibleObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DestructibleObject : MonoBehaviour
+{
+    public int health;
+    public enum AttackType 
+    {
+    Physical,
+    Fire
+    };
+
+    public virtual void takeDamage(int damage)
+    {
+        health -= damage;
+        healthCheck();
+    }
+    public virtual void takeDamage(AttackType attackType, int damage)
+    {
+
+        health -= damage;
+        healthCheck();
+    }
+    public void healthCheck()
+    {
+        if(health <= 0)
+        {
+            onDestroy(); 
+        }
+    }
+    public virtual void onDestroy()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Destructibles/DestructiblePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiblePlayer : DestructibleObject
+{
+    public override void takeDamage(AttackType attackType, int damage)
+    {        
+     //   base.takeDamage(attackType, damage);
+    }
+    public override void takeDamage(int damage)
+    {
+       // base.takeDamage(damage);
+    }
+    public override void onDestroy()
+    {
+        base.onDestroy();
+    }
+    
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
