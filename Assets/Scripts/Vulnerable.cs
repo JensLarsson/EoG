2f722cfae9f1b602cf2ebc84932362a3077acee3
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-11-16 10:39:55 +0100

    Derp

--- Assets/Scripts/Vulnerable.cs
+++ Assets/Scripts/Vulnerable.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Vulnerable : MonoBehaviour
 {
-    public virtual void EnterState()
+    private int Health = 3; //Detta kan behöva ändras i fall andra beteenden läggs till
+    protected VulnerabilityStateMachine vulMan;
+
+    public Vulnerable(VulnerabilityStateMachine vulMan)
+    {
+        this.vulMan = vulMan;
+    }
+    public virtual void EnterState() //Detta kan vara överflödgt
     {
 
     }
@@ -16,18 +23,37 @@ public class Vulnerable : MonoBehaviour
     {
 
     }
-    public virtual void Hit()
+    public virtual void Hit(int damage)
     {
-
+        Health -= damage;
+        vulMan.ChangeState(VulnerabilityStateMachine.Vulnerability.InVunerable);
     }
 }
 
 public class InVulnerable : Vulnerable
 {
-    public override void Hit()
+    float invulerableTime = 1.0f;
+    float currentTime = 0.0f;
+    public InVulnerable(VulnerabilityStateMachine vulMan, float invulerableTime) : base(vulMan)
     {
-
+        this.invulerableTime = invulerableTime;
     }
-
+    public override void EnterState()
+    {
+        currentTime = 0.0f;
+        base.EnterState();
+    }
+    public override void Hit(int damage)
+    {
+    }
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (
+            invulerableTime > currentTime)
+        {
+            vulMan.ChangeState(VulnerabilityStateMachine.Vulnerability.Vulnerable);
+        }
+    }
 
 }
