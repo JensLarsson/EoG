9ee92fb28b11b3e89cb371fcd12e3f088b037686
PretentiousPanda <42602418+PretentiousPanda@users.noreply.github.com>
2019-11-17 20:50:38 +0100

    EventTrigger

--- Assets/Scripts/Events/EventTrigger.cs
+++ Assets/Scripts/Events/EventTrigger.cs
@@ -1,35 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventTrigger : MonoBehaviour
 {
     [Header("Event Data")]
-    public string eventCall;
+    public string[] eventCalls = new string[1];
     public EventParameter eventParameter;
+    public UnityEvent action;
 
     [Header("Trigger Options")]
-    [SerializeField] actions postTriggerAction = actions.Nothing;
-    enum actions { Nothing = 0, DeleteObject, DeactivateTrigger };
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] Actions postTriggerAction = Actions.Nothing;
+    enum Actions { Nothing = 0, DeleteObject, DeactivateTrigger };
+    [SerializeField] Interactions interactionType = Interactions.onButtonPress;
+    enum Interactions { onEnter = 0, onButtonPress, onLeave }
+
+
+    void Engage()
     {
-        if (collision.tag == "Player")
+        foreach (string eventCall in eventCalls)
         {
             if (eventCall != "")
             {
                 EventManager.TriggerEvent(eventCall, eventParameter);
+                action.Invoke();
             }
             switch (postTriggerAction)
             {
-                case actions.DeleteObject:
+                case Actions.DeleteObject:
                     Destroy(this.gameObject);
                     break;
 
-                case actions.DeactivateTrigger:
+                case Actions.DeactivateTrigger:
                     Destroy(this);
                     break;
             }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (interactionType == Interactions.onEnter && collision.tag == "Player")
+        {
+            Engage();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (interactionType == Interactions.onEnter && collision.gameObject.tag == "Player")
+        {
+            Engage();
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (interactionType == Interactions.onButtonPress && collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            Engage();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (interactionType == Interactions.onButtonPress && collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            Engage();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (interactionType == Interactions.onLeave && collision.gameObject.tag == "Player")
+        {
+            Engage();
+        }
+    }
+
 }
---
+++ Assets/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+        EventManager.Subscribe("StartTimer", StartTimer);
+    }
+    private void OnDisable()
+    {
+        EventManager.UnSubscribe("StartTimer", StartTimer);
+    }
+
+    void StartTimer(EventParameter eParam)
+    {
+        StartCoroutine(RunTimer(eParam.floatParam, eParam));
+    }
+
+    IEnumerator RunTimer(float time, EventParameter eParam)
+    {
+        text.enabled = true;
+        float clock = 0.0f;
+        while (clock < time)
+        {
+            clock += Time.deltaTime;
+            text.text = ((float)System.Math.Round(clock * 100f) / 100f).ToString();
+            yield return null;
+        }
+        text.enabled = false;
+        EventManager.TriggerEvent(eParam.stringParam, eParam);
+
+    }
+}
